70b4e396865307164336bea9b35449b717c357a8
maryoud2000 <maryoud2000@gmail.com>
2022-03-09 21:08:25 +0000

    player interaction

--- App.js
+++ App.js
@@ -1,58 +1,109 @@
 class Pet{
-    constructor(petName) {
+    constructor(petName, hunger, bored, thirst) {
         this._petName = petName;
+        this._hunger = 100;
+        this._bored = 100;
+        this._thirst = 100;
     }
+get petName() {
+    return this._petName;
+}
+get hunger() {
+    return this._hunger;
+}
+get bored() {
+    return this._bored;
+}
+get thirst() {
+    return this._thirst;
 }
 
+feed() {
+    this._hunger ++;
+    this._thirst -= 0.5;
+    this.bored += 0.5;
+}
+drink() {
+    this._thirst ++;
+    this._hunger += 0.5;
+    this._bored += 0.5;
+}
+play() {
+    this._bored ++;
+    this._hunger -= 0.5;
+    this._thirst -= 0.5;
+}
+relax() {
+    this._hunger --;
+    this._thirst --;
+    this.bored --;
+}
+}
 class Dog extends Pet {
-    constructor(petName, hunger){
+    constructor(petName, walk){
         super(petName);
-        this._petName = petName;
-        this._hunger = hunger;
+        this._walk = walk;
     }
-
     dogAction () {
-        if (this._hunger >= 75) {
-            console.log(`so hungery, please feed him...`);
+        this._walk ++;
+        this._bored --;
+        this._hunger ++;
+        this._thirst ++;
+        if (this._walk > 75) {
+            console.log(`${this.petName} is tired now, need to relax...`);
         }
-        else{
-            console.log(`is replete`);
-        }}
-}
+        else if (this._bored < 50) {
+            console.log(`${this.petName} is happy now...`);
+        }
+        else if (this._hunger > 75) {
+            console.log(`${this.petName} so hungery, please feed him...`);
+        }
+        else if (this._thirst >75) {
+            console.log(`${this.petName} is thirsty, +please give him something to drink...`);
+        }
+        else {
 
+        }
+    }
+}
 class Cat extends Pet {
-    constructor(petName, bored) {
+    constructor(petName) {
         super(petName);
-        this._petName = petName;
-        this._bored = bored;
     }
     catAction () {
-        if (this._bored >= 75) {
-            return `wants play with or walk outside...`;
+        if (this._bored > 75) {
+            return `${this.petName} wants play with or walk outside...`;
         }
         else{
-            return `is happy and giving cuddles`;
+            return `${this.petName} is happy and giving cuddles`;
         }}
 }
-
 class Rabbit extends Pet {
-    constructor(petName, thirst){
+    constructor(petName){
         super(petName);
-        this._petName = petName;
-        this._thirst = thirst;
     }
     rabbitAction () {
         if (this._thirst >= 75) {
-            return `is looking for something to drink...`;
+            return `${this.petName} is looking for something to drink...`;
         }
         else{
-            return `is playing`;
+            return `${this.petName} is playing`;
         }}
 }
 
-let dog = new Dog("Sue", 60); 
-let cat = new Cat("rob", 100); 
-let rabbit = new Rabbit("cici", 30);
-dog.dogAction();
-cat.catAction();
-rabbit.rabbitAction();
+function alive(cyberPet){
+    cyberPet.intervalID = setInterval(()=>{
+        if (cyberPet.bored == true || cyberPet.drink == true || cyberPet.feed == true) {
+            clearInterval(cyberPet.intervalID);
+        } else {
+            cyberPet.relax();
+            console.log(cyberPet);
+        }
+    }, 3000)
+};
+// let dog = new Dog("Sue", 60); 
+// let cat = new Cat("rob", 100); 
+// let rabbit = new Rabbit("cici", 30);
+// dog.dogAction();
+// cat.catAction();
+// rabbit.rabbitAction();
